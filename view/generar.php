603047ec34df4d2149dc27120d2e9d06fe808274
pperez <patodeath@gmail.com>
2017-11-05 21:56:45 -0300

    Se cambió la lista de tags, que el usuario escribia, por una lista desplegable

--- view/generar.php
+++ view/generar.php
@@ -9,65 +9,64 @@
 
         var i = 0;
         function addTag(e, value){
-            if(e.keyCode == 13 && value != ""){
-                $.ajax({
-                    url: "../controller/getTagId.php",
-                    data: {
-                        tagName: value
-                    },
-                    success: function( tagId ) {
-                        var men = document.getElementById("mensaje");
-                        var tag = document.getElementById("tag");
+            $.ajax({
+                url: "../controller/getTagId.php",
+                data: {
+                    tagName: value
+                },
+                success: function( tagId ) {
+                    var men = document.getElementById("mensaje");
 
-                        if(tagId != -1){
-                            var generado = document.getElementById("generado");
-                            
-                            if(!generado.innerHTML.includes(value)){
-                                generado.innerHTML += "<input type='hidden' name='id_"+i+"' value='"+tagId+"'>";
-                                generado.innerHTML += "<input type='text' value='"+value+"' readonly>";
-                                men.innerHTML = "";
-                                i++;
+                    if(tagId != -1){
+                        var generado = document.getElementById("generado");
+                        
+                        if(!generado.innerHTML.includes(value)){
+                            generado.innerHTML += "<input type='hidden' name='id_"+i+"' value='"+tagId+"'>";
+                            generado.innerHTML += "<input type='text' value='"+value+"' readonly>";
+                            men.innerHTML = "";
+                            i++;
 
-                                var cantTags = document.getElementById("cantTags");
-                                cantTags.value = i;
-                            }else{
-                                men.innerHTML = "El tag ya está en la lista";
-                            }
+                            var cantTags = document.getElementById("cantTags");
+                            cantTags.value = i;
                         }else{
-                            men.innerHTML = "El tag no existe";
+                            men.innerHTML = "El tag ya está en la lista";
                         }
-                        tag.value = "";
+                    }else{
+                        men.innerHTML = "El tag no existe";
                     }
-                });
-            }
+                }
+            });
         }
         </script>
     </head>
     <body>
         <h1>Generar Preguntas</h1>
-        <input id="tag" list="tags" onkeyup="addTag(event, this.value)" placeholder="Escribe tu tag">
+
         
-        <datalist id="tags">
-            <?php
-            require_once("../model/Data.php");
-
-            $d = new Data();
-
-            foreach($d->getTags() as $t){
-                echo "<option value='".$t->nombre."'>";
-            }
-            ?>
-        </datalist>
 
         <form id="formTags" action="../controller/generarPreguntas.php" method="post">
             <input name="cantidad" placeholder="Cantidad de preguntas:">
+            <select onclick="addTag(event, this.value)">
+                <option value="">Seleccione un tag...</option>
+                <?php
+                require_once("../model/Data.php");
+
+                $d = new Data();
+
+                foreach($d->getTags() as $t){
+                    echo "<option value='".$t->nombre."'>".$t->nombre."</option>";
+                }
+                ?>
+            </select>
             <input type="hidden" value="0" name="cantTags" id="cantTags">
             <br>
             <div id="generado"></div>
             <br>
             <input type="submit" value="Generar">
         </form>
+
         <div id="mensaje"></div>
+
         <a href="index.php">Volver</a>
     </body>
 </html>
